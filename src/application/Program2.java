c54d71ad75d2a3a70c85b9de06227ed5e57f4b94
jairoy <jairo.yenecura@gmail.com>
2023-10-31 16:03:35 -0300

    findById implementation for departmentDaoJDBC

---
+++ src/application/Program2.java
@@ -0,0 +1,18 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {		
+
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+		Department department = departmentDao.findById(4);
+		
+		System.out.println(department);
+
+	}
+}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,41 +1,69 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
-	
+
 	private Connection conn;
-	
+
 	public DepartmentDaoJDBC(Connection conn) {
-		this.conn = conn;		
+		this.conn = conn;
 	}
 
 	@Override
 	public void insert(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement("SELECT * FROM " + "department " + "WHERE ID = ? ");
+			st.setInt(1, id);
+
+			rs = st.executeQuery();
+
+			if (rs.next()) {
+				Department dep = new Department(rs.getInt("Id"), rs.getString("Name"));
+				return dep;
+			} else {
+				return null;
+			}
+
+		} catch (SQLException e) {
+			
+			throw new DbException(e.getMessage());
+			
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
+
 	}
 
 	@Override
